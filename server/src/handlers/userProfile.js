b9d4aad15057f9810bacdc34c14bc24264a56021
Adolfo Simeone <adolfo.simeone.001@gmail.com>
2021-08-01 23:12:25 -0300

    Add new endpoint stubs

---
+++ server/src/handlers/userProfile.js
@@ -0,0 +1,12 @@
+const handle_GET_userProfile = (req, res) => {
+
+}
+
+const handle_POST_userProfile = (req, res) => {
+
+}
+
+module.exports = {
+    handle_GET_userProfile,
+    handle_POST_userProfile
+}
---
+++ server/src/handlers/userScore.js
@@ -0,0 +1,17 @@
+const handle_GET_userScore = (req, res) => {
+
+}
+
+const handle_POST_viewInfo = (req, res) => {
+
+}
+
+const handle_GET_info = (req, res) => {
+
+}
+
+module.exports = {
+    handle_GET_userScore,
+    handle_POST_viewInfo,
+    handle_GET_info
+}
