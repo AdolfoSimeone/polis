37c55ffef689f92111e1e83696c90ed617fbc8ee
Adolfo Simeone <adolfo.simeone.001@gmail.com>
2021-08-11 14:08:15 -0300

    Add sql imports to new handlers

--- server/src/handlers/userScore.js
+++ server/src/handlers/userScore.js
@@ -1,12 +1,22 @@
-const handle_GET_userScore = (req, res) => {
+const pg = require("../db/pg-query");
+const pgQuery = pg.query;
+const pgQuery_readOnly = pg.query_readOnly;
+const pgQueryP = pg.queryP;
+const pgQueryP_metered = pg.queryP_metered;
+const pgQueryP_metered_readOnly = pg.queryP_metered_readOnly;
+const pgQueryP_readOnly = pg.queryP_readOnly;
+const pgQueryP_readOnly_wRetryIfEmpty = pg.queryP_readOnly_wRetryIfEmpty;
+const SQL = require("../db/sql");
+
+const handle_GET_userScore = async (req, res) => {
 
 }
 
-const handle_POST_viewInfo = (req, res) => {
+const handle_POST_viewInfo = async (req, res) => {
 
 }
 
-const handle_GET_info = (req, res) => {
+const handle_GET_info = async (req, res) => {
 
 }
 
